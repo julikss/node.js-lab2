a6b262742093dd0a4d1d0e46fb77da35054ba20a
Anastasiia Sanina <saninaanstasia97@gmail.com>
2023-03-07 04:27:18 +0200

    Get news texts

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const url = 'https://tsn.ua/news';
 
+let newsArray = [];
 
-request('https://tsn.ua/news', (error, response, html) => {
+request(url, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
     const siteHeading = $('.c-section__title');
-    const siteContainer = $('.c-card__body__embed');
-    const output = $('.c-card__title');
+    const links = $('.c-card__link');
 
     console.log(siteHeading.text());
-    //console.log(siteContainer.text());
-    console.log(output.text());
+
+    links.each((i, link) => {
+      requestNewsPage($(link).attr("href"), (newsText) => newsArray.push({heading: $(link).text(), text: newsText}));
+    })
   }
 });
+
+const requestNewsPage = (urlPage, callback) => {
+  request(urlPage, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      const newsText = $('.c-article__body p');
+    
+      callback(newsText.text());      
+    }
+  });
+}
