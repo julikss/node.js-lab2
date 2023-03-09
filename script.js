61351b78cec2881f3549339a0c674844d17eec06
DESKTOP-TQL0JT6\juliks <julie.lehenka@gmail.com>
2023-03-09 12:04:07 +0200

    Add func to display news by click

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const getFiles = async () => {
+const namesBlock = document.querySelector('.namesBlock');
+const newsBlock = document.querySelector('.newsBlock');
+
+const displayNews = (arrOfNews) => {
+  let listOfFiles = '';
   
+  for (const el of arrOfNews) {
+      listOfFiles += `
+        <div class="names">${Object.keys(el)[0]}</div>
+      `
+    }
+  
+  namesBlock.innerHTML = listOfFiles;
+
+  const names = document.querySelectorAll('.names');
+
+  for (const name of names) {
+    name.addEventListener('click', () => {
+      for (const el of arrOfNews) {
+        if (name.innerHTML === Object.keys(el)[0]) {
+          newsBlock.innerHTML =Object.values(el)
+        }
+      }
+    })
+  }
+}
+
+
+const getFiles = async () => {
   let response = await fetch("http://localhost:8000/")
   .then(res => res.json());
-  console.log(response)
+  displayNews(response);
 }
 
 
